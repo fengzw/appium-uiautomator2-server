75ba09b669711d8e34d44fbb8333473937f6753a
hzliujq <hzliujq@corp.netease.com>
2017-07-21 15:10:04 +0800

    获取Source优先使用UiDevice.dumpWindowHierarchy

--- app/src/main/java/io/appium/uiautomator2/handler/Source.java
+++ app/src/main/java/io/appium/uiautomator2/handler/Source.java
@@ -2,7 +2,11 @@ package io.appium.uiautomator2.handler;
 
 import org.w3c.dom.Document;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.StringWriter;
+import java.nio.charset.Charset;
 
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
@@ -20,6 +24,8 @@ import io.appium.uiautomator2.utils.Logger;
 import io.appium.uiautomator2.utils.ReflectionUtils;
 import io.appium.uiautomator2.utils.XMLHierarchy;
 
+import static io.appium.uiautomator2.utils.Device.getUiDevice;
+
 /**
  * Get page source. Return as string of XML doc
  */
@@ -31,26 +37,42 @@ public class Source extends SafeRequestHandler {
 
     @Override
     public AppiumResponse safeHandle(IHttpRequest request) {
+
+        String xmlString = null;
+
+        // 先尝试使用 getUiDevice().dumpWindowHierarchy(baos);
         try {
             ReflectionUtils.clearAccessibilityCache();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            getUiDevice().dumpWindowHierarchy(baos);
+            xmlString = baos.toString("UTF-8");
+            baos.close();
+        } catch (Exception e) {
+            Logger.error("Exception while performing UiDevice.dumpWindowHierarchy to UTF-8 string ", e);
+        }
 
-            final Document doc = (Document) XMLHierarchy.getFormattedXMLDoc();
-            final TransformerFactory tf = TransformerFactory.newInstance();
-            final StringWriter writer = new StringWriter();
-            Transformer transformer = tf.newTransformer();
-            transformer.transform(new DOMSource(doc), new StreamResult(writer));
-            String xmlString = writer.getBuffer().toString();
+        if ( null != xmlString && xmlString.length() > 0 ) {
             return new AppiumResponse(getSessionId(request), WDStatus.SUCCESS, xmlString);
+        } else {
+            try {
+                final Document doc = (Document) XMLHierarchy.getFormattedXMLDoc();
+                final TransformerFactory tf = TransformerFactory.newInstance();
+                final StringWriter writer = new StringWriter();
+                Transformer transformer = tf.newTransformer();
+                transformer.transform(new DOMSource(doc), new StreamResult(writer));
+                xmlString = writer.getBuffer().toString();
+                return new AppiumResponse(getSessionId(request), WDStatus.SUCCESS, xmlString);
 
-        } catch (final TransformerConfigurationException e) {
-            Logger.error("Unable to handle the request:" + e);
-            return new AppiumResponse(getSessionId(request), WDStatus.UNKNOWN_ERROR, "Something went terribly wrong while converting xml document to string:" + e);
-        } catch (final TransformerException e) {
-            Logger.error("Unable to handle the request:" + e);
-            return new AppiumResponse(getSessionId(request), WDStatus.UNKNOWN_ERROR, "Could not parse xml hierarchy to string: " + e);
-        } catch (UiAutomator2Exception e) {
-            Logger.error("Exception while performing LongPressKeyCode action: ", e);
-            return new AppiumResponse(getSessionId(request), WDStatus.UNKNOWN_ERROR, e);
+            } catch (final TransformerConfigurationException e) {
+                Logger.error("Unable to handle the request:" + e);
+                return new AppiumResponse(getSessionId(request), WDStatus.UNKNOWN_ERROR, "Something went terribly wrong while converting xml document to string:" + e);
+            } catch (final TransformerException e) {
+                Logger.error("Unable to handle the request:" + e);
+                return new AppiumResponse(getSessionId(request), WDStatus.UNKNOWN_ERROR, "Could not parse xml hierarchy to string: " + e);
+            } catch (UiAutomator2Exception e) {
+                Logger.error("Exception while performing LongPressKeyCode action: ", e);
+                return new AppiumResponse(getSessionId(request), WDStatus.UNKNOWN_ERROR, e);
+            }
         }
 
     }
